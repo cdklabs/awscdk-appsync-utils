494db80433230e51f93f8a194d5269fd4ea55426
Momo Kornher <kornherm@amazon.co.uk>
2022-11-29 14:18:52 +0000

    chore: fix versioning

--- .projenrc.js
+++ .projenrc.js
@@ -1,8 +1,13 @@
 const { awscdk } = require('projen');
+
+// CDK and alpha package versions need to be in sync
+const CDK_VERSION = '2.51.1';
+const APPSYNC_ALPHA_VERSION = `${CDK_VERSION}-alpha.0`;
+
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Mitchell Valine',
   authorAddress: 'mitchellvaline@yahoo.com',
-  cdkVersion: '2.51.1',
+  cdkVersion: CDK_VERSION,
   defaultReleaseBranch: 'main',
   name: 'awscdk-appsync-utils',
   repositoryUrl: 'https://github.com/cdklabs/awscdk-appsync-utils.git',
@@ -12,7 +17,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   devDeps: [
     '@aws-cdk/aws-appsync-alpha',
   ],
-  peerDeps: ['@aws-cdk/aws-appsync-alpha'],
+  peerDeps: [`@aws-cdk/aws-appsync-alpha@${APPSYNC_ALPHA_VERSION}`],
   // packageName: undefined,  /* The "name" in package.json. */
 });
 project.synth();
